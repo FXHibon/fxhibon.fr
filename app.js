01589c7e6aae802d7b9c81e4bbf10f7b0d6a57c1
FXHibon <hibonfx@gmail.com>
2016-02-03 14:27:04 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by fx on 03/02/16.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log('Listening on %d', port);
+});
+
